fa2f04be009fa5b9e02d8c39ff830c9fbcd8b89e
zackwitz <zackwitz@gmail.com>
2019-05-09 19:46:09 -0400

    CSS finally working

--- ui.py
+++ ui.py
@@ -1,16 +1,19 @@
 from flask import Flask, request, render_template, redirect, url_for
-from game import *
+import os
+#from game import *
+TEMPLATE_DIR = os.path.abspath('templates')
+STATIC_DIR = os.path.abspath('static')
 
 
-app = Flask(__name__)
-id_attrib_dict, tfidf, tfs, id_to_row, title_to_id = init_game()
+app = Flask(__name__, template_folder=TEMPLATE_DIR, static_folder=STATIC_DIR)
+#id_attrib_dict, tfidf, tfs, id_to_row, title_to_id = init_game()
 
 class Path:
     def __init__(self, s, e):
         self.start = s
         self.end = e
 
-
+"""
 @app.route('/<string:start_title>_to_<string:end_title>')
 def search(start_title, end_title):
     start_id = title_to_id[start_title]
@@ -18,7 +21,7 @@ def search(start_title, end_title):
     lineage = priority_beam_search(start_id, end_id, 10, dummy_sim)
     return render_template('search.html',
                             start=start_title, end=end_title, lineage=lineage)
-
+"""
 
 @app.route('/', methods = ["POST", "GET"])
 def add():
